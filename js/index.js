31915e37822c9fbc44e32432c83eee7849ba043d
WebDev SHORTS <44138280+WebDevSHORTS@users.noreply.github.com>
2019-06-30 20:27:12 +0300

    Updated skill bars

--- js/index.js
+++ js/index.js
@@ -1,21 +1,13 @@
 //  !Progress bar
 function initSkills() {
-    $(".skill-progress span").each(function() {
-        let bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        let bottom_of_window = $(window).scrollTop() + $(window).height();
-        let myVal = $(this).attr("data-value");
-        let title = $(this)
-            .parent()
-            .parent()
-            .children(":first-child");
-        if (bottom_of_window > bottom_of_object) {
+    $(window).scroll(function() {
+        $(".skill-progress span").each(function() {
+            let myVal = $(this).attr("data-value");
+
             $(this).css({
                 width: myVal
             });
-            title.css({
-                width: myVal
-            });
-        }
+        });
     });
 }
 
@@ -24,5 +16,5 @@ let waypoint = new Waypoint({
     handler: function() {
         initSkills();
     },
-    offset: 200
+    offset: "50%"
 });
