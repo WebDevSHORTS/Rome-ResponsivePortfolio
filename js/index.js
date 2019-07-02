b19fb8558cd6ebee1c391c1a1285e755b3092321
WebDev SHORTS <44138280+WebDevSHORTS@users.noreply.github.com>
2019-07-02 23:20:10 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,21 +1,13 @@
 //  !Progress bar
 function initSkills() {
-    $(".skill-progress span").each(function() {
-        let bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        let bottom_of_window = $(window).scrollTop() + $(window).height();
-        let myVal = $(this).attr("data-value");
-        let title = $(this)
-            .parent()
-            .parent()
-            .children(":first-child");
-        if (bottom_of_window > bottom_of_object) {
+    $(window).scroll(function() {
+        $(".skill-progress span").each(function() {
+            let myVal = $(this).attr("data-value");
+
             $(this).css({
                 width: myVal
             });
-            title.css({
-                width: myVal
-            });
-        }
+        });
     });
 }
 
@@ -24,5 +16,21 @@ let waypoint = new Waypoint({
     handler: function() {
         initSkills();
     },
-    offset: 200
+    offset: "50%"
+});
+
+//  !Fixed navbar on scroll
+$(window).scroll(function() {
+    console.log("scrolling");
+    if ($(window).scrollTop() > 50) {
+        $(".navbar").addClass("fixed");
+    } else {
+        $(".navbar").removeClass("fixed");
+    }
+});
+
+//  !Handling Hamburger menu
+let $hamburger = $(".hamburger");
+$hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
 });
